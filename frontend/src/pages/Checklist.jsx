b4489ecc43b9f0f06a52107902c7512fb5209a8a
Derek Rodriguez <derekirodriguez05@gmail.com>
2025-06-09 17:33:38 -0400

    added custom modals

--- frontend/src/pages/Checklist.jsx
+++ frontend/src/pages/Checklist.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
+import ViewChecklistModal from '../components/ViewChecklistModal';
+import CreateChecklistModal from '../components/CreateChecklistModal';
 
 function Checklist() {
   return (
     <div className="checklist">
       <h1>Checklist</h1>
       <p>This is the checklist page.</p>
+      <ViewChecklistModal />
+      <CreateChecklistModal />
     </div>
   );
 } export default Checklist;
